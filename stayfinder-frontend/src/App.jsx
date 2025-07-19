0fddcd7def4a1359007bc44325802155efd12bd8
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2025-07-19 06:51:59 +0530

    Update App.jsx

--- stayfinder-frontend/src/App.jsx
+++ stayfinder-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register"; 
-import ListingDetail from "./pages/ListingDetail"; 
+import ListingDetail from "./pages/ListingDetail";  
 import HostDashboard from "./pages/HostDashboard";
 import PrivateRoute from "./components/PrivateRoute";
 import AddListing from "./pages/AddListing"; 
