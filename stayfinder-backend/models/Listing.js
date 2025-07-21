6cb779461ea377cbdfa0bc2c2d9bb5c7167e7c87
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2025-07-21 16:50:59 +0530

    Update Listing.js

--- stayfinder-backend/models/Listing.js
+++ stayfinder-backend/models/Listing.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const listingSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true }, 
-  price: { type: Number, required: true },
+  price: { type: Number, required: true }, 
   location: { type: String, required: true },
   image: { type: String, required: true }, 
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
