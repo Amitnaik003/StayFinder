f41c24f798d5224e1718c1b0dd0fe81eba4f8b2c
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2025-07-29 08:08:34 +0530

    Update payments.js

--- stayfinder-backend/routes/payments.js
+++ stayfinder-backend/routes/payments.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Stripe from "stripe";
-
+ 
 const router = express.Router(); 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); // Use environment variable   
 
