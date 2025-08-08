945c18283f5edf4a6e85ec91fa10b7d14876b2b8
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2025-08-08 07:59:33 +0530

    Update EditListing.jsx

--- stayfinder-frontend/src/pages/EditListing.jsx
+++ stayfinder-frontend/src/pages/EditListing.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import API from "../api/axios";  
-import { DateRange } from "react-date-range";
+import { DateRange } from "react-date-range"; 
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 
