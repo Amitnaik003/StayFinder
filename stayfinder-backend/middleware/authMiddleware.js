f14f744d39533fb327178516be1dd73ac47fec89
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2026-01-06 09:51:47 +0530

    Update authMiddleware.js

--- stayfinder-backend/middleware/authMiddleware.js
+++ stayfinder-backend/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET; // Use environment variable
 
 export const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization; 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized" });
   }
